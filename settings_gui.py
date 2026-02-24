2c9deb60fed759eabddd231c35ae6f8a3a8222fc
addielponce <205451387+addielponce@users.noreply.github.com>
2026-02-23 21:32:26 -0500

    Refactor into a class to create more rows

--- settings_gui.py
+++ settings_gui.py
@@ -10,9 +10,66 @@ from aqt.qt import (
     QPushButton,
     QSpinBox,
     QVBoxLayout,
+    QWidget,
 )
 
 PACKAGE_NAME = "anki-progress-circle"
+
+
+class ColorPickerRow(QWidget):
+    def __init__(self, label_text, initial_color, initial_opacity, parent=None):
+        super().__init__(parent)
+        self._color = QColor(initial_color)
+
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        label = QLabel(label_text)
+        self._button = QPushButton()
+        self._button.setFixedHeight(self._button.sizeHint().height() - 6)
+        self._button.clicked.connect(self._pick_color)
+
+        alpha_label = QLabel("Opacity:")
+        self._opacity_spin = QSpinBox()
+        self._opacity_spin.setRange(0, 100)
+        self._opacity_spin.setSingleStep(5)
+        self._opacity_spin.setSuffix("%")
+        self._opacity_spin.setValue(initial_opacity)
+        self._opacity_spin.valueChanged.connect(self._refresh_button)
+
+        layout.addWidget(label)
+        layout.addWidget(self._button)
+        layout.addWidget(alpha_label)
+        layout.addWidget(self._opacity_spin)
+        self.setLayout(layout)
+
+        self._refresh_button()
+
+    def _refresh_button(self):
+        opacity = self._opacity_spin.value() / 100
+        r, g, b = self._color.red(), self._color.green(), self._color.blue()
+        self._button.setStyleSheet(f"background-color: rgba({r}, {g}, {b}, {opacity});")
+
+    def _pick_color(self):
+        color = QColorDialog.getColor(self._color, self, "Pick color")
+        if color.isValid():
+            self._color = color
+            self._refresh_button()
+
+    @property
+    def color(self):
+        return self._color.name(QColor.NameFormat.HexRgb)
+
+    @property
+    def opacity(self):
+        return self._opacity_spin.value()
+
+    def set_color(self, color):
+        self._color = QColor(color)
+        self._refresh_button()
+
+    def set_opacity(self, opacity):
+        self._opacity_spin.setValue(opacity)
 
 
 class SettingsDialog(QDialog):
@@ -96,8 +153,8 @@ class SettingsDialog(QDialog):
             self._refresh_button()
 
     def _save(self):
-        self.config["main_color"] = self._color.name(QColor.NameFormat.HexRgb)
-        self.config["main_color_opacity"] = self.opacity_spin.value()
+        self.config["main_color"] = self.main_color_picker.color
+        self.config["main_color_opacity"] = self.main_color_picker.opacity
         mw.addonManager.writeConfig(PACKAGE_NAME, self.config)
         self.accept()
         from . import update_progress
@@ -108,9 +165,8 @@ class SettingsDialog(QDialog):
         defaults = mw.addonManager.addonConfigDefaults(PACKAGE_NAME)
         if defaults:
             self.config = defaults
-            self._color = QColor(defaults["main_color"])
-            self.opacity_spin.setValue(defaults.get("main_color_opacity", 100))
-            self._refresh_button()
+            self.main_color_picker.set_color(defaults["main_color"])
+            self.main_color_picker.set_opacity(defaults.get("main_color_opacity", 100))
 
 
 def open_settings():
